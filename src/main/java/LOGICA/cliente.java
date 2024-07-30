92256ac0317d34529bbd64513383e2545f6261e7
Daniel <daniel92enfermeria@gmail.com>
2024-07-29 20:10:58 -0500

    iniciando

--- src/main/java/LOGICA/cliente.java
+++ src/main/java/LOGICA/cliente.java
@@ -4,12 +4,74 @@ package LOGICA;
 
 public class cliente {
     
-    private int id;
-    private int id;
-    private int id;
-    private int id;
-    private int id;
-    private int id;
+    private int Id;
+    private String Nombre;
+    private String Apellido;
+    private int Documento;
+    private int Telefono;
+    private String Direccion;
+
+    public cliente() {
+    }
+
+    public cliente(int Id, String Nombre, String Apellido, int Documento, int Telefono, String Direccion) {
+        this.Id = Id;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Documento = Documento;
+        this.Telefono = Telefono;
+        this.Direccion = Direccion;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public int getDocumento() {
+        return Documento;
+    }
+
+    public void setDocumento(int Documento) {
+        this.Documento = Documento;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
+    
             
             
             
