d657a3b0ed203eae452e5265e615c458944e3905
Daniel <daniel92enfermeria@gmail.com>
2024-07-29 19:39:00 -0500

    iniciando

---
+++ src/main/java/LOGICA/cliente.java
@@ -0,0 +1,18 @@
+
+package LOGICA;
+
+
+public class cliente {
+    
+    private int id;
+    private int id;
+    private int id;
+    private int id;
+    private int id;
+    private int id;
+            
+            
+            
+            
+            
+}
